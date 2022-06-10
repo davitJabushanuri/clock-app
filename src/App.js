d3b814b741408f6b50f10f1e5f7d808f83a03a9b
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-11 01:05:26 +0400

    make geolocationAPI fetch request on laod and save it in geolocation state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React, { useState, useEffect } from 'react'
 import './scss/main.scss'
 
 function App() {
-	return <div className='App'>clock app</div>
+	const [geolocation, setGeolocation] = useState({})
+
+	const randomQuoteApi =
+		'https://programming-quotes-api.herokuapp.com/Quotes/random'
+
+	const worldTimeApi = 'https://worldtimeapi.org/api/timezone/Asia/Tbilisi'
+
+	const GEOLOCATION_API_KEY = process.env.REACT_APP_GEOLOCATION_API_KEY
+
+	const geolocationApi = `https://api.ipbase.com/v2/info?apikey=${GEOLOCATION_API_KEY}&language=en&ip=`
+
+	const fetchGeolocation = async url => {
+		const res = await fetch(url)
+		const data = await res.json()
+		setGeolocation(data.data)
+	}
+	console.log(geolocation)
+
+	useEffect(() => {
+		fetchGeolocation(geolocationApi)
+	}, [])
+
+	return <div className='App'></div>
 }
 
 export default App
