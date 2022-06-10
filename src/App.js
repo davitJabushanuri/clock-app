cafbd46a43fef4ae81973f56c999f2d63b0dc913
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-11 03:17:02 +0400

    setup quotes component to fetch and display data on load and on button click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import Quotes from './components/Quotes'
 import './scss/main.scss'
 
 function App() {
 	const [geolocation, setGeolocation] = useState({})
-
-	const randomQuoteApi =
-		'https://programming-quotes-api.herokuapp.com/Quotes/random'
+	const [time, setTime] = useState()
 
 	const worldTimeApi = 'https://worldtimeapi.org/api/timezone/Asia/Tbilisi'
 
@@ -18,13 +17,31 @@ function App() {
 		const data = await res.json()
 		setGeolocation(data.data)
 	}
-	console.log(geolocation)
+
+	const fetchWorldTime = async url => {
+		const res = await fetch(url)
+		const data = await res.json()
+		const date = new Date(data.unixtime * 1000)
+		var hours = date.getHours()
+		var minutes = '0' + date.getMinutes()
+		var formattedTime = hours + ':' + minutes.substr(-2)
+		setTime(formattedTime)
+		console.log(formattedTime)
+	}
 
 	useEffect(() => {
 		fetchGeolocation(geolocationApi)
-	}, [])
+	}, [geolocationApi])
 
-	return <div className='App'></div>
+	useEffect(() => {
+		fetchWorldTime('http://worldtimeapi.org/api/ip')
+	}, [geolocation])
+
+	return (
+		<div className='App'>
+			<Quotes />
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Quotes.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import RefreshIcon from '../assets/desktop/icon-refresh.svg'
+
+const Quotes = () => {
+	const [quote, setQuote] = useState()
+
+	const randomQuoteApi =
+		'https://programming-quotes-api.herokuapp.com/Quotes/random'
+
+	const getRandomQuote = async () => {
+		const res = await fetch(randomQuoteApi)
+		const data = await res.json()
+		setQuote({
+			quote: data.en,
+			author: data.author,
+		})
+	}
+
+	useEffect(() => {
+		getRandomQuote()
+	}, [])
+
+	return (
+		<section className='quotes'>
+			<div className='quotes__container'>
+				<div className='quotes__container__quote'>{quote && quote.quote}</div>
+				<div className='quotes__container__author'>{quote && quote.author}</div>
+				<button onClick={getRandomQuote} className='quotes__container__button'>
+					<img src={RefreshIcon} alt='' />
+				</button>
+			</div>
+		</section>
+	)
+}
+
+export default Quotes
