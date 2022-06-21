7800b9a37953c276ab95b04eb22d1dccf695720d
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-21 20:00:48 +0400

    set background depending on device width and time of day

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from 'react'
 import Quotes from './components/Quotes'
 import './scss/main.scss'
 
+// backgrounds
+import mobileDayBackground from './assets/mobile/bg-image-daytime.jpg'
+import mobileNightBackground from './assets/mobile/bg-image-nighttime.jpg'
+import tabletDayBackground from './assets/tablet/bg-image-daytime.jpg'
+import tabletNightBackground from './assets/tablet/bg-image-nighttime.jpg'
+import desktopDayBackground from './assets/desktop/bg-image-daytime.jpg'
+import desktopNightBackground from './assets/desktop/bg-image-nighttime.jpg'
+
 function App() {
 	const [geolocation, setGeolocation] = useState({})
 	const [time, setTime] = useState()
+	const [isDay, setIsDay] = useState(false)
+	const [greeting, setGreeting] = useState('Good morning')
+	const [background, setBackground] = useState(mobileDayBackground)
 
-	const worldTimeApi = 'https://worldtimeapi.org/api/timezone/Asia/Tbilisi'
-
-	const GEOLOCATION_API_KEY = process.env.REACT_APP_GEOLOCATION_API_KEY
-
-	const geolocationApi = `https://api.ipbase.com/v2/info?apikey=${GEOLOCATION_API_KEY}&language=en&ip=`
+	const worldTimeApi = 'http://worldtimeapi.org/api/ip'
+	const geolocationApi = `https://api.ipbase.com/v2/info?apikey=${process.env.REACT_APP_GEOLOCATION_API_KEY}&language=en&ip=`
 
 	const fetchGeolocation = async url => {
 		const res = await fetch(url)
@@ -26,19 +34,46 @@ function App() {
 		var minutes = '0' + date.getMinutes()
 		var formattedTime = hours + ':' + minutes.substr(-2)
 		setTime(formattedTime)
-		console.log(formattedTime)
+		hours > 6 && hours < 18 ? setIsDay(true) : setIsDay(false)
+		hours > 6 && hours <= 12
+			? setGreeting('Good morning')
+			: hours > 12 && hours <= 18
+			? setGreeting('Good afternoon')
+			: setGreeting('Good evening')
+	}
+
+	const setBackgroundImage = () => {
+		if (window.innerWidth < 768) {
+			isDay
+				? setBackground(mobileDayBackground)
+				: setBackground(mobileNightBackground)
+		} else if (window.innerWidth < 992) {
+			isDay
+				? setBackground(tabletDayBackground)
+				: setBackground(tabletNightBackground)
+		} else {
+			isDay
+				? setBackground(desktopDayBackground)
+				: setBackground(desktopNightBackground)
+		}
 	}
 
 	useEffect(() => {
 		fetchGeolocation(geolocationApi)
+		fetchWorldTime(worldTimeApi)
 	}, [geolocationApi])
 
 	useEffect(() => {
-		fetchWorldTime('http://worldtimeapi.org/api/ip')
-	}, [geolocation])
+		setBackgroundImage()
+	}, [isDay])
 
 	return (
-		<div className='App'>
+		<div
+			className='App'
+			style={{
+				backgroundImage: `url(${background})`,
+			}}
+		>
 			<Quotes />
 		</div>
 	)
